102532c3a20c00ae9d5d15129732f6c9d466ab87
MZA <last.randomness@gmail.com>
2019-01-09 04:27:56 -0800

    Initial Commit

---
+++ put it in text List/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  put it in text List
+//
+//  Created by TikTak on 1/9/19.
+//  Copyright © 2019 TikTak. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
